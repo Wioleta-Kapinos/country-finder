4a62534e7f87e735851c5fab4879256fadc46126
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2023-01-14 11:18:52 +0100

    renederowanie listy krajów

--- src/index.js
+++ src/index.js
@@ -7,9 +7,42 @@ let searchInput = document.querySelector("#search-box");
 const listOfCountries = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
 
-searchInput.addEventListener("input", _.debounce(() => {
-    fetchCountries()
-    .then((name) => console.log(name))
-    .catch((error) => console.log(error));
-    }, DEBOUNCE_DELAY)
-);
+function renederListOfCountry(name) {
+    const list = name.map((el, i) => {
+        return `<li class="country-item"><img class="country-image" src=${name[i].flags.svg} /><p class="country-name">${name[i].name.official}</p></li>`;
+    }).join("");
+    listOfCountries.innerHTML = list;
+}
+
+function renederInfoOfCountry(name) {
+    renederListOfCountry(name);
+    const info = name.map((el, i) => {
+        return `<p class="country-capital"><b>Capital:</b> ${name[i].capital}</p>
+       <p class="country-population"><b>Population:</b> ${name[i].population}</p>
+       <p class="country-languages"><b>Languages:</b> ${name[i].languages}</p>`
+   }).join("");
+   countryInfo.innerHTML = info;
+}
+
+const searchCountry = event => {
+    let name = event.target.value.trim();
+    if (name === "") {
+        listOfCountries.innerHTML = "";
+        countryInfo.innerHTML = "";
+        return;
+    }
+    fetchCountries(name)
+     .then((name) => {
+        if (name.length > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name."); 
+        } else if (name.length >= 2 && name.length <= 10) {
+            renederListOfCountry(name);    
+        } else if (name.length = 1) {
+            renederInfoOfCountry(name);
+        }
+    })
+     .catch((error) => {
+        (Notiflix.Notify.failure("Oops, there is no country with that name"));
+     });
+}
+searchInput.addEventListener("input", _.debounce(searchCountry,DEBOUNCE_DELAY));
